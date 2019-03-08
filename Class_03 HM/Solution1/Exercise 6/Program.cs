1cabe76826c0e50374816901b42ebdf64174f9d5
Martin <petrovskiq@icloud.com>
2019-03-08 12:45:40 +0100

    Homework

---
+++ Class_03 HM/Solution1/Exercise 6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+          
+
+
+            Console.Write("Enter Name");
+            Console.Write("\n");
+            string[] arrayNames = new string[1];
+            string yesOrNo;
+
+            
+
+                 
+
+            do
+            {
+                Array.Resize(ref arrayNames, arrayNames.Length + 1);
+                arrayNames[arrayNames.Length - 1] = Console.ReadLine();
+                Console.Write("Do you want another name?");
+                yesOrNo = Console.ReadLine();
+
+
+            } while (yesOrNo == "y");
+
+            Console.Write("All Names:");
+            for (int i = 0; i < arrayNames.Length; i++)
+            {
+                Console.Write(arrayNames[i] + "\n");
+            }
+
+            
+           
+
+           
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Class_04 HM/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter 20 Integers");
+            int[] arrayInt = new int[20];
+
+            for (var i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInt[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Your array!");
+
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                Console.WriteLine(arrayInt[i]);
+                
+                if (arrayInt[i] == 0) {
+
+                    Console.WriteLine("Skipped");
+                    continue;
+                }
+                if (Math.Abs(arrayInt[i]
+                    ).ToString().Length == 3) {
+                    break;
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
