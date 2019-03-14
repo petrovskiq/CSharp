2eb6138504554bbe3d530a115b412062dcaaf218
Martin <petrovskiq@icloud.com>
2019-03-14 15:29:15 +0100

    Exercises from Class07

---
+++ Class_07 Exercises/Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
